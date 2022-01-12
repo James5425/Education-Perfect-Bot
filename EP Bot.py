61e5703a7c21e830469f085e8b2eab6faf5b3774
James Kosinar <73284017+James5425@users.noreply.github.com>
2022-01-12 14:15:20 +1100

    Update EP Bot.py

--- EP Bot.py
+++ EP Bot.py
@@ -4,18 +4,21 @@ from selenium.webdriver import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import StaleElementReferenceException
 
 # view https://dev.to/ruthmoog/dealing-with-error-chromedriver-cannot-be-opened-because-the-developer-cannot-be
 # -verified-1897 if any issues with apple quarantine
-
+#https://app.educationperfect.com/app/Japanese/6230459/4299030/list-starter
 # Path to webdriver
 try:
     driver = webdriver.Chrome('ENTER THE PATH TO THR SELENIUM DRIVER DOWNLOAD FROM https://chromedriver.chromium.org/home')
 except WebDriverException:
     print("You need to download the webdriver for your computer and then add it to PATH")
     exit()
+
 # Load Website
 website = input("Enter start page URL (This is the page with all the questions and answers)\n>")
+
 driver.get(website)
 
 # Login Info
@@ -33,6 +36,7 @@ i = 1
 
 a = 1
 
+g = 1
 
 # Logs into EP
 def login(u, p):
@@ -53,8 +57,10 @@ def page_numbers():
     except NoSuchElementException:
         pages = 1
 
+    return pages
 
-def main(i1, a1):
+
+def infinit():
     time.sleep(3)
 
     # Scroll Page and Selects option as infinite
@@ -66,111 +72,181 @@ def main(i1, a1):
     element.click()
     time.sleep(2)
 
-    # For when only one page of questions
-    if pages == 1:
-        while True:
-            try:
-                # Adds the questions and answers into arrays
-                question = driver.find_element_by_xpath(
-                    f'/html/body/main/div[3]/div/student-app-wrapper/div[1]/div[2]/div/ui-view/div/div[2]/div/div['
-                    f'2]/div[4]/div[1]/div[1]/div[{i1}]/div[1]/div[1]').get_attribute(
-                    'textContent').strip()
 
-                # If there is a ; the question delete it and everything before it as EP Bot doesn't read this properly
-                for x in question:
-                    if x == ";":
-                        sep = ';'
-                        question = question.split(sep, 1)[1].strip()
+def one_page(i1, readOrWrite):
+    print("just one")
+    while True:
+        try:
+            # Adds the questions and answers into arrays
+            question = driver.find_element_by_xpath(
+                f'/html/body/main/div[3]/div/student-app-wrapper/div[1]/div[2]/div/ui-view/div/div[2]/div/div['
+                f'2]/div[4]/div[1]/div[1]/div[{i1}]/div[1]/div[1]').get_attribute(
+                'textContent').strip()
+
+            # If there is a ; the question delete it and everything before it as EP Bot doesn't read this properly
+            for x in question:
+                if x == ";":
+                    sep = ';'
+                    question = question.split(sep, 1)[1].strip()
+                else:
+                    question = question
+
+            answer = driver.find_element_by_xpath(
+                f'/html/body/main/div[3]/div/student-app-wrapper/div[1]/div[2]/div/ui-view/div/div[2]/div/div['
+                f'2]/div[4]/div[1]/div[1]/div[{i1}]/div[1]/div[2]').get_attribute(
+                'textContent').strip()
+
+            if readOrWrite == 2:
+                for b in answer:
+                    if b == ";":
+                        sep = ";"
+                        answer = answer.split(sep, 1)[1].strip()
                     else:
-                        question = question
+                        answer = answer
+            else:
+                pass
+            questions.append(question)
+            answers.append(answer)
+            i1 += 1
+        except:
+            # For when there are no more questions
+            break
 
-                answer = driver.find_element_by_xpath(
-                    f'/html/body/main/div[3]/div/student-app-wrapper/div[1]/div[2]/div/ui-view/div/div[2]/div/div['
-                    f'2]/div[4]/div[1]/div[1]/div[{i1}]/div[1]/div[2]').get_attribute(
-                    'textContent').strip()
-                questions.append(question)
-                answers.append(answer)
-                i1 += 1
-            except:
-                # For when there are no more questions
-                break
+def more_than_one_page(i1, a1, pages, readOrWrite):
+    b = 1
 
-    # For when more than one page of questions
-    elif pages > 1:
-        b = 1
+    while b == 1:
+        try:
+            # Adds the questions and answers into arrays
+            question = driver.find_element_by_xpath(
+                f'/html/body/main/div[3]/div/student-app-wrapper/div[1]/div[2]/div/ui-view/div/div[2]/div/div['
+                f'2]/div[4]/div[1]/div[1]/div[{i1}]/div[1]/div[1]').get_attribute(
+                'textContent').strip()
 
-        while b == 1:
-            try:
-                # Adds the questions and answers into arrays
-                question = driver.find_element_by_xpath(
-                    f'/html/body/main/div[3]/div/student-app-wrapper/div[1]/div[2]/div/ui-view/div/div[2]/div/div['
-                    f'2]/div[4]/div[1]/div[1]/div[{i1}]/div[1]/div[1]').get_attribute(
-                    'textContent').strip()
+            # If there is a ; the question delete it and everything before it as EP Bot doesn't read this properly
+            for x in question:
+                if x == ";":
+                    sep = ';'
+                    question = question.split(sep, 1)[1].strip()
+                else:
+                    question = question
 
-                # If there is a ; the question delete it and everything before it as EP Bot doesn't read this properly
-                for x in question:
-                    if x == ";":
-                        sep = ';'
-                        question = question.split(sep, 1)[1].strip()
+            answer = driver.find_element_by_xpath(
+                f'/html/body/main/div[3]/div/student-app-wrapper/div[1]/div[2]/div/ui-view/div/div[2]/div/div['
+                f'2]/div[4]/div[1]/div[1]/div[{i1}]/div[1]/div[2]').get_attribute(
+                'textContent').strip()
+
+            if readOrWrite == 2:
+                for b in answer:
+                    if b == ";":
+                        sep = ";"
+                        answer = answer.split(sep, 1)[1].strip()
                     else:
-                        question = question
+                        answer = answer
+            else:
+                pass
 
-                answer = driver.find_element_by_xpath(
-                    f'/html/body/main/div[3]/div/student-app-wrapper/div[1]/div[2]/div/ui-view/div/div[2]/div/div['
-                    f'2]/div[4]/div[1]/div[1]/div[{i1}]/div[1]/div[2]').get_attribute(
-                    'textContent').strip()
-                questions.append(question)
-                answers.append(answer)
-                i1 += 1
-            except:
-                # For when there are no more questions
-                driver.find_element_by_xpath(
-                    '/html/body/main/div[3]/div/student-app-wrapper/div[1]/div[2]/div/ui-view/div/div[2]/div/div['
-                    '2]/div[1]/div[2]/div/div[3]').click()
-                a1 += 1
-                i1 = 1
-                time.sleep(3)
-                if a == pages:
-                    b = 2
-    else:
-        print("Invalid Page Number")
+            questions.append(question)
+            answers.append(answer)
+            i1 += 1
+        except:
+            # For when there are no more questions
+            driver.find_element_by_xpath(
+                '/html/body/main/div[3]/div/student-app-wrapper/div[1]/div[2]/div/ui-view/div/div[2]/div/div['
+                '2]/div[1]/div[2]/div/div[3]').click()
+            a1 += 1
+            i1 = 1
+            time.sleep(3)
+            if a1 == pages:
+                b = 2
 
+
+def start():
     # Starts EP Answering
     time.sleep(4)
     driver.find_element_by_id("start-button-main").click()
-    time.sleep(4)
+    time.sleep(2)
+
+
+def answer():
+    g = 1
     while True:
 
-        # Reads questions
-        task_question = driver.find_element_by_xpath(
-            '/html/body/main/div[3]/div/student-app-wrapper/div[1]/div[2]/div[1]/ui-view/div[1]/div[2]/div/div/div['
-            '1]/div[2]/div/div[2]/div[2]/span[2]/span').text
         try:
-            # Finds answer to question
-            task_answer_index = questions.index(task_question)
-            task_answer = (answers[task_answer_index])
-        except:
-            # Finds answer to question
-            task_answer_index = answers.index(task_question)
-            task_answer = (questions[task_answer_index])
-
-        # Removes everything in the answer after ; as EP bot does not need this
-        try:
-            sep = ';'
-            stripped = task_answer.split(sep, 1)[0]
-            # Enters answer into text box
-            enter_answer = driver.find_element_by_xpath(
+            # Reads questions
+            task_question = driver.find_element_by_xpath(
                 '/html/body/main/div[3]/div/student-app-wrapper/div[1]/div[2]/div[1]/ui-view/div[1]/div[2]/div/div/div['
-                '2]/div[2]/game-lp-answer-input/div/div[2]/input')
-            enter_answer.send_keys(stripped)
-            enter_answer.send_keys(Keys.ENTER)
-            time.sleep(1.5)
+                '1]/div[2]/div/div[2]/div[2]/span[2]/span').text
+            try:
+                # Finds answer to question
+                task_answer_index = questions.index(task_question)
+                task_answer = (answers[task_answer_index])
+            except:
+                # Finds answer to question
+                task_answer_index = answers.index(task_question)
+                task_answer = (questions[task_answer_index])
+
+            # Removes everything in the answer after ; as EP bot does not need this
+            try:
+                sep = ';'
+                stripped = task_answer.split(sep, 1)[0]
+                # Enters answer into text box
+                enter_answer = driver.find_element_by_xpath(
+                    '/html/body/main/div[3]/div/student-app-wrapper/div[1]/div[2]/div[1]/ui-view/div[1]/div[2]/div/div/div['
+                    '2]/div[2]/game-lp-answer-input/div/div[2]/input')
+                enter_answer.send_keys(stripped)
+                enter_answer.send_keys(Keys.ENTER)
+                prev_answer = stripped
+                if read_write == 2:
+                    if driver.find_element_by_xpath('/html/body/main/div[3]/div/student-app-wrapper/div[1]/div[2]/div[1]/ui-view/div[1]/div[2]/div/div/div[''1]/div[2]/div/div[2]/div[2]/span[2]/span').text == task_question:
+                        enter_answer.send_keys(Keys.ENTER)
+                    else:
+                        pass
+                else:
+                    pass
+                time.sleep(3)
+            except NoSuchElementException:
+                print("All Questions Finished")
         except NoSuchElementException:
-            print("All Questions Finished")
+            g += 1
+            if g != 5:
+                answer()
+            else:
+                break
+
+def reading_or_writing():
+    global read_write
+    read_write = int(input("Would you like the EP Bot to do the reading or writing section? (Reading = 1 / Writing = 2)\n>"))
+    if read_write == 1:
+        pass
+    elif read_write == 2:
+        driver.find_element_by_xpath('/html/body/main/div[3]/div/student-app-wrapper/div[1]/div[2]/div/ui-view/div/div[2]/div/div[1]/div[3]/div[1]/div[2]/ul/li[4]').click()
+    else:
+        print("Please Enter a valid option")
+    return read_write
+
+def main(i1, a1):
+    login(username, password)
+    time.sleep(3)
+
+    reading_or_writing()
+    time.sleep(1)
+    infinit()
+    page_numbers()
+    print(pages)
+
+    if pages == 1: # For when only one page of questions
+        print('1')
+        one_page(i1, read_write)
+
+    elif pages > 1:  # For when more than one page of questions
+        print('>1')
+        more_than_one_page(i1, a1, pages, read_write)
+
+    start()
+    time.sleep(2)
+    answer()
 
 
 if __name__ == '__main__':
-    login(username, password)
-    time.sleep(3)
-    page_numbers()
     main(i, a)
